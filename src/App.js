9a27699c9c48ec5b126271005d290ba36b7689cd
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-03-23 11:16:11 +0000

    feat: add data state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
 // https://api.quotable.io/random?tags=technology,famous-quotes
 function App() {
-  const mainColor= `#${Math.floor(Math.random()*1000000)}`
+  const [mainColor, setMainColor] = useState("")
+  const [data, setData] = useState({})
+  const [clicked, setClicked] = useState(0)
   
+   async function handleNewQuote(){
+    const toFadeOut = document.querySelectorAll('.toFadeOut')
+    toFadeOut.forEach((elem)=>elem.style.animation="fadeOut 1s")
+    htmlRoot.style.animation = "fadeOut 1s"
+      let myPromise = new Promise((resolve)=>{
+        setTimeout(()=>(resolve('ok')), 1000) 
+        
+      
+      })
+    await myPromise
+    setClicked((prev)=> prev+1)
+    
+  }
   const htmlRoot = document.querySelector(":root")
   htmlRoot.style.setProperty('--main-color', mainColor)
   // console.log(htmlRoot.getPropertyValue('height'))
   useEffect(()=>{
-    fetch('https://api.quotable.io/random?tags=technology,famous-quotes')
+    
+    fetch('https://api.quotable.io/random?')
     .then(res=> res.json())
-    .then(res=> console.log(res))
-  },[])
+    .then(res=> setData(res))
+    setMainColor(`#${Math.ceil(Math.random()*999999)}`)
+  },[clicked])
+  console.log(data)
   return (
-    <div className="App">
-      <h1 className='quote--h1'><i className="fa-solid fa-quote-left"></i>When I let go of what I am, I become what I might be.</h1>
-      <h5 className='author--h5'>- Lao Tzu</h5>
-      <div className='bottom-container'>
-        <i className="fa-brands fa-square-twitter"></i>
-        <i className="fa-brands fa-square-tumblr"></i>
-        <button className='newQuote-btn' >
-          New quote
-        </button>
+    <div className='background-div'>
+      <div key={clicked} className="App">
+        <h1 className='quote--h1 toFadeOut' ><i className="fa-solid fa-quote-left"></i>{data.content}</h1>
+        <h5 className='author--h5 toFadeOut'>- {data.author}</h5>
+        <div className='bottom-container'>
+          <i className="fa-brands fa-square-twitter .toFadeOut" ></i>
+          <i className="fa-brands fa-square-tumblr .toFadeOut"></i>
+          <button className='newQuote-btn .toFadeOut' onClick={handleNewQuote} >
+            New quote
+          </button>
+        </div>
       </div>
     </div>
   );
